30873dc33c3866d756d765f8b1ab1947eb6d49fb
Noel Feliciano <feliciano.noel@gmail.com>
2014-09-24 18:40:23 -0700

    'finalized' meal model, added 'placeID' which will be taken from Google. 'key' will be a combination of placeID and time, and it will be generated on the client

--- app/server/models/meal.js
+++ app/server/models/meal.js
@@ -2,5 +2,10 @@ var mongoose = require('mongoose');
 
 //Model is named 'Meal'
 module.exports = mongoose.model('Meal', {
-	name: String
+	key: { type: "string", unique: true },
+	placeID: String,
+	numPeople: Number,
+	time: Date,
+	people: Array,
+	active: { type: Boolean, default: true }
 });
