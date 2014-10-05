529227672c1ad03750d5d811c8d0d3afd2dd6839
Kevin Gill <hkevgill@hotmail.com>
2014-10-05 14:35:58 -0700

    Updates images of google and facebook login, adhering to policies

--- app/client/js/googleLogin.js
+++ app/client/js/googleLogin.js
@@ -26,6 +26,6 @@ function signinCallback(authResult) {
     	//   "user_signed_out" - User is signed-out
     	//   "access_denied" - User denied access to your app
     	//   "immediate_failed" - Could not automatically log in the user
-    	console.log('Sign-in state: ' + authResult['error']);
+    	// console.log('Sign-in state: ' + authResult['error']);
     }
 }
