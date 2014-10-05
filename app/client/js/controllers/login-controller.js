92dc99fc911983dc0679de4128aa8f7d7d4f9d5b
Lloyd Montgomery <lloydrmontgomery@gmail.com>
2014-10-04 18:25:49 -0700

    Added show and hide for divs

--- app/client/js/controllers/login-controller.js
+++ app/client/js/controllers/login-controller.js
@@ -1,7 +1,8 @@
 app.controller('loginController', ['$scope', '$resource', 
 	function ($scope, $resource) {
 		$scope.dates = []
-
+		$scope.hideStartEating = false
+		$scope.hideUserInfo = true
 
 		$scope.submitUserData = function() {
 			$scope.dates.push({ month: $scope.month,  
@@ -9,5 +10,10 @@ app.controller('loginController', ['$scope', '$resource',
 								year: $scope.year,
 								occupation: $scope.occupation,
 								description: $scope.description});
+		}
+
+		$scope.switchDivs = function() {
+			$scope.hideStartEating = !$scope.hideStartEating
+			$scope.hideUserInfo = !$scope.hideUserInfo
 		}
 }]);
