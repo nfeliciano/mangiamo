a333376c4f382888a26a4e3cc1cf60fb540a2e26
Kevin Gill <hkevgill@hotmail.com>
2014-10-08 17:56:54 -0700

    When a user logs in with either FB or G+, we now have their unique ID and Name.

--- app/client/js/googleLogin.js
+++ app/client/js/googleLogin.js
@@ -16,9 +16,30 @@ function render() {
 	gapi.signin.render('googleLogin', additionalParams);
 }
 
+function getUserInfo() {
+	// Step 4: Load the Google+ API
+	gapi.client.load('plus', 'v1').then(function() {
+		// Step 5: Assemble the API request
+		var request = gapi.client.plus.people.get({
+			'userId': 'me'
+		});
+		// Step 6: Execute the API request
+		request.then(function(resp) {
+			console.log(resp.result.id);
+			console.log(resp.result.displayName);
+		}, 
+		function(reason) {
+			console.log('Error: ' + reason.result.error.message);
+		});
+	});
+}
+
+
 function signinCallback(authResult) {
 	if (authResult['status']['signed_in']) {
 		// Update the app to reflect a signed in user
+		getUserInfo();
+
     } 
     else {
     	// Update the app to reflect a signed out user
