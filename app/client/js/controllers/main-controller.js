f704945b5b606afc7dfdcd859fd77af819fd0cbf
Noel Feliciano <feliciano.noel@gmail.com>
2014-10-18 16:16:25 -0700

    removed 'fix' from yesterday where it would relocate to login if the user tries to go to map without having logged in. it was causing issues where if you create a user, it would redirect back to login (async issues)

--- app/client/js/controllers/main-controller.js
+++ app/client/js/controllers/main-controller.js
@@ -7,13 +7,6 @@ app.controller('mainController', ['$scope', '$resource', '$location', '$modal', 
 			zoom: 14
 		}
 
-		$scope.init = function() {
-			if (!userService.isUserLoggedIn()) {
-				$location.path('login').replace();
-			}
-		}
-		$scope.init();
-
 		// initializes the google map and populates it with food places
 		$scope.initialize = function() {
 			$scope.map = new google.maps.Map(document.getElementById('mapCanvas'), mapOptions);
