b453a6020859382fb0efe8610804ccebc24b46de
Noel Feliciano <feliciano.noel@gmail.com>
2014-10-22 20:18:12 -0700

    google authentication seems to work

--- app/client/js/controllers/login-controller.js
+++ app/client/js/controllers/login-controller.js
@@ -103,4 +103,59 @@ app.controller('loginController', ['$scope', '$location', 'userService',
 			js.src = "//connect.facebook.net/en_US/sdk.js";
 			fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));
+
+
+		/* Google Integration Stuff */
+		(function() {
+			var po = document.createElement('script');
+			po.type = 'text/javascript'; 
+			po.async = true;
+			po.src = 'https://apis.google.com/js/client:plusone.js?onload=render';
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(po, s);
+		})();
+
+		/* Executed when the APIs finish loading */
+		render = function() {
+			// Additional params
+			var additionalParams = {
+				'theme' : 'dark'
+			};
+			gapi.signin.render('googleLogin', additionalParams);
+		}
+
+		getUserInfo = function() {
+			// Step 4: Load the Google+ API
+			gapi.client.load('plus', 'v1').then(function() {
+				// Step 5: Assemble the API request
+				var request = gapi.client.plus.people.get({
+					'userId': 'me'
+				});
+				// Step 6: Execute the API request
+				request.then(function(resp) {
+					console.log(resp.result.id);
+					console.log(resp.result.displayName);
+				}, 
+				function(reason) {
+					console.log('Error: ' + reason.result.error.message);
+				});
+			});
+		}
+
+
+		signinCallback = function(authResult) {
+			if (authResult['status']['signed_in']) {
+				// Update the app to reflect a signed in user
+				getUserInfo();
+
+		    } 
+		    else {
+		    	// Update the app to reflect a signed out user
+		    	// Possible error values:
+		    	//   "user_signed_out" - User is signed-out
+		    	//   "access_denied" - User denied access to your app
+		    	//   "immediate_failed" - Could not automatically log in the user
+		    	// console.log('Sign-in state: ' + authResult['error']);
+		    }
+		}
 }]);
