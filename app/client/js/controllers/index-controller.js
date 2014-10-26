51de85e24f7e69ca50d03191cff233c019d6ae70
Lloyd Montgomery <lloydrmontgomery@gmail.com>
2014-10-25 18:12:32 -0700

    Working meal buddies hide and show.  Going to attempt changing classes instead to hopefully allow transitions to work.

--- app/client/js/controllers/index-controller.js
+++ app/client/js/controllers/index-controller.js
@@ -1,6 +1,9 @@
 app.controller('indexController', ['$scope', '$location', 'userService',
 	function ($scope, $location, userService) {
 
+		$scope.mapClass = 'col-sm-12';
+		$scope.hideMealBuddies = true;
+
 		// This allows the initial redirect when they come to the 
 		// page based on whether or not they are logged in
 		$scope.init = function() {
@@ -16,5 +19,17 @@ app.controller('indexController', ['$scope', '$location', 'userService',
 		$scope.logout = function() {
 			userService.logoutUser();
 			$location.path('login').replace();
+		}
+
+		$scope.toggleMealBuddies = function() {
+			if ($scope.mapClass == 'col-sm-12') {
+				$scope.hideMealBuddies = false;
+				$scope.mapClass = 'col-sm-9';
+			}
+			else {
+				$scope.hideMealBuddies = true;
+				$scope.mapClass = 'col-sm-12';
+			}
+
 		}
 }]);
